33bbc33613d190517cfda987bb5e2d59a0d0df20
Rabot9ga <danila.klenin@gmail.com>
2016-05-15 12:39:31 +0300

    Add Generator.py, InputData.xlsx
    Generator make OutputCode.txt (Script for Weintek) and
    OutputVar.xlsx (Variables for Weintek)
    
    Generator v0.99 not tested on Weinek

---
+++ Generator.py
@@ -0,0 +1,224 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.cell import get_column_letter
+
+inputFile = 'InputData.xlsx'
+
+wb = load_workbook(filename = inputFile)
+sheet = wb['Vars']
+
+# Заполнение списка структур
+col = 'H'
+structList = []
+row = 2
+cell = col + str(row)
+while str(sheet[cell].value) != 'None' and row < 1000:
+#    print(sheet[cell].value)
+    structList.append(str(sheet[cell].value))
+    row = row + 1
+    cell = col + str(row)
+
+print (structList)
+
+# Заполнение списка названий экранов
+col = 'I'
+screenList = []
+row = 2
+cell = col + str(row)
+while str(sheet[cell].value) != 'None' and row < 1000:
+#    print(sheet[cell].value)
+    screenList.append(str(sheet[cell].value))
+    row = row + 1
+    cell = col + str(row)
+
+print (screenList)
+
+startLW = sheet['F1'].value
+startLB = sheet['F2'].value
+screenID = sheet['F3'].value
+varPrefix = sheet['F4'].value
+screenNameVar = sheet['F5'].value
+PLCName = sheet['F6'].value 
+
+# Заполняем матрицу переменных
+varRow = []
+varMatrix = []
+row = 2
+col = 1
+cell = get_column_letter(col) + str(row)
+while str(sheet[cell].value) != 'None' and row < 1000:
+    for col in range(1, 4):
+        cell = get_column_letter(col) + str(row)
+        varRow.append(sheet[cell].value)
+    row += 1
+    cell = get_column_letter(col) + str(row)
+    varMatrix.append(varRow)
+#    print(varRow)
+    varRow = []
+
+print(varMatrix)
+
+index = 0
+for typeVar in varMatrix:
+    varMatrix[index][1] = typeVar[1].upper()
+    index += 1
+print (varMatrix)
+
+
+# Определяем типы переменных
+index = 0
+for variable in varMatrix:
+    if (variable[1] == 'LB') or (variable[1] == 'BOOL'):
+        varMatrix[index][1] = 1  #1 - тип Бит
+    if (variable[1] == 'WORD') or (variable[1] == 'INT') or (variable[1] == 'LW') or (variable[1] == 'INTEGER'):
+        varMatrix[index][1] = 2  #2 - тип слово
+    if (variable[1] == 'REAL') or (variable[1] == 'DINT') or (variable[1] == 'DWORD'):
+        varMatrix[index][1] = 3  #3 - тип двойное слово
+    index += 1
+print(varMatrix)
+
+
+# Создаем книгу
+wb = Workbook()
+dest_filename = 'OutputVar.xlsx'
+ws = wb.active
+ws.title = 'HMI_Vars'
+
+row = 1
+col = 1
+lwIndex = startLW
+lbIndex = startLB
+
+for nameVar in varMatrix:
+    cell = get_column_letter(col) + str(row)
+    ws[cell] = varPrefix + 'Old_' + nameVar[0]
+    cell = get_column_letter(col + 1) + str(row)
+    ws[cell] = 'Local HMI'
+
+    cell = get_column_letter(col + 2) + str(row)
+    if nameVar[1] == 1:
+        ws[cell] = 'LB'
+        cell = get_column_letter(col + 3) + str(row)
+        ws[cell] = str(lbIndex)
+        lbIndex += 1
+    if nameVar[1] == 2:
+        ws[cell] = 'LW'
+        cell = get_column_letter(col + 3) + str(row)
+        ws[cell] = str(lwIndex)
+        lwIndex += 1
+    if nameVar[1] == 3:
+        ws[cell] = 'LW'
+        cell = get_column_letter(col + 3) + str(row)
+        ws[cell] = str(lwIndex)
+        lwIndex += 2
+
+    cell = get_column_letter(col + 4) + str(row)
+    ws[cell] = ''
+    cell = get_column_letter(col + 5) + str(row)
+    ws[cell] = 'Неопределенный'
+    row += 1
+
+wb.save(filename = dest_filename)
+
+startScript = """
+macro_command main()
+
+short LcParameterSetNumber, TempShort, TempOldShort, ConstZero
+unsigned int TempInt, TempOldInt, ConstIntZero
+bool ConstTrue, ConstFalse, TempBool, TempOldBool
+
+char ScreenName[20] = ""
+
+
+ConstTrue  = true
+ConstFalse = false
+ConstZero  = 0
+
+"""
+str1 = 'GetData(LcParameterSetNumber, "Local HMI", "' + screenID + '", 1)'
+
+codeFileName = 'OutputCode.txt'
+outputFile = open(codeFileName,'w')
+
+outputFile.write(startScript+str1+'\n')
+
+currStructNumber = 0
+endStructs = len(structList)
+for j in structList:
+    currNumVar = 0
+    if currStructNumber == 0:
+        str1 = 'if LcParameterSetNumber == ' + str(currStructNumber + 1) + ' then\n'
+        print(str1)
+        #outputFile.write(str1+'\n')
+    else:
+        str1 = 'else if LcParameterSetNumber == ' + str(currStructNumber + 1) + ' then\n'
+        print(str1)
+
+    str2 = '    StringCopy("' + screenList[currStructNumber] + '", ScreenName[0])\n'
+    str3 = '    StringSet(ScreenName[0], "Local HMI", "' + screenNameVar + '", 20)\n'
+    outputFile.write(str1 + str2 + str3 + '\n')
+    for i in varMatrix:
+        if i[2] == 1:
+            if i[1] == 1:
+                str1 = '    GetData(TempOldBool, "Local HMI", "' + varPrefix + 'Old_' + i[0] + '", 1)\n'
+                str2 = '    GetData(TempBool, "Local HMI", "' + varPrefix + i[0] + '", 1)\n'
+                str3 = '    if 	(TempOldBool <> TempBool) then\n'
+                str4 = '        SetData(TempBool, "' + PLCName + '", "' + structList[currStructNumber] + '.' + i[0] + '", 1)\n'
+                str5 = '    else\n'
+                str6 = '        GetData(TempBool, "' + PLCName + '", "' + structList[currStructNumber] + '.' + i[0] + '", 1)\n'
+                str7 = '        SetData(TempBool, "Local HMI", "' + varPrefix + i[0] + '", 1)\n'
+                str8 = '    end if\n'
+                str9 = '    GetData(TempBool, "Local HMI", "' + varPrefix + i[0] + '", 1)\n'
+                str10 = '    SetData(TempBool, "Local HMI", "' + varPrefix + 'Old_' + i[0] + '", 1)\n'
+            if i[1] == 2:
+                str1 = '    GetData(TempOldShort, "Local HMI", "' + varPrefix + 'Old_' + i[0] + '", 1)\n'
+                str2 = '    GetData(TempShort, "Local HMI", "' + varPrefix + i[0] + '", 1)\n'
+                str3 = '    if 	(TempOldShort <> TempShort) then\n'
+                str4 = '        SetData(TempShort, "' + PLCName + '", "' + structList[currStructNumber] + '.' + i[0] + '", 1)\n'
+                str5 = '    else\n'
+                str6 = '        GetData(TempShort, "' + PLCName + '", "' + structList[currStructNumber] + '.' + i[0] + '", 1)\n'
+                str7 = '        SetData(TempShort, "Local HMI", "' + varPrefix + i[0] + '", 1)\n'
+                str8 = '    end if\n'
+                str9 = '    GetData(TempShort, "Local HMI", "' + varPrefix + i[0] + '", 1)\n'
+                str10 = '    SetData(TempShort, "Local HMI", "' + varPrefix + 'Old_' + i[0] + '", 1)\n'
+            if i[1] == 3:
+                str1 = '    GetData(TempOldInt, "Local HMI", "'+ varPrefix + 'Old_' + i[0] + '", 1)\n'
+                str2 = '    GetData(TempInt, "Local HMI", "'+ varPrefix + i[0] + '", 1)\n'
+                str3 = '    if 	(TempOldInt <> TempInt) then\n'
+                str4 = '        SetData(TempInt, "' + PLCName + '", "'+ structList[currStructNumber] + '.' + i[0] + '", 1)\n'
+                str5 = '    else\n'
+                str6 = '        GetData(TempInt, "' + PLCName + '", "'+ structList[currStructNumber] + '.' + i[0] + '", 1)\n'
+                str7 = '        SetData(TempInt, "Local HMI", "'+ varPrefix + i[0] + '", 1)\n'
+                str8 = '    end if\n'
+                str9 = '    GetData(TempInt, "Local HMI", "'+ varPrefix + i[0] + '", 1)\n'
+                str10 = '    SetData(TempInt, "Local HMI", "'+ varPrefix + 'Old_' + i[0] + '", 1)\n'
+            print(str1+str2+str3+str4+str5+str6+str7+str8+str9+str10)
+            outputFile.write(str1+str2+str3+str4+str5+str6+str7+str8+str9+str10+'\n')
+        else:
+            if i[1] == 1:
+                str1 = '    GetData(TempBool, "' + PLCName + '", "' + structList[currStructNumber] + '.' + i[0] + '", 1)\n'
+                str2 = '    SetData(TempBool, "Local HMI", "' + varPrefix + i[0] + '", 1)\n'
+            if i[1] == 2:
+                str1 = '    GetData(TempShort, "' + PLCName + '", "' + structList[currStructNumber] + '.' + i[0] + '", 1)\n'
+                str2 = '    SetData(TempShort, "Local HMI", "' + varPrefix + i[0] + '", 1)\n'
+            if i[1] == 3:
+                str1 = '    GetData(TempInt, "' + PLCName + '", "' + structList[currStructNumber] + '.' + i[0] + '", 1)\n'
+                str2 = '    SetData(TempInt, "Local HMI", "' + varPrefix + i[0] + '", 1)\n'
+            print(str1+str2)
+            outputFile.write(str1+str2+'\n')
+        print(i)
+        currNumVar += 1
+    currStructNumber += 1
+    if currStructNumber == endStructs:
+        str1 = 'end if\n'
+        str2 = '\n'
+        str3 = 'end macro_command\n'
+        print(str1+str2+str3)
+        outputFile.write(str1+str2+str3+'\n')
+
+outputFile.close()
+
+print('Создан файл импорта переменных в Weintek "' + dest_filename + '"')
+print('Создан файл макроса для Weintek "' + codeFileName + '"')
+print('\n')
+input('Press any key...')
--- Weintek_Parameters.py
+++ Weintek_Parameters.py
@@ -38,6 +38,8 @@ macro_command main()
 short LcParameterSetNumber, TempShort, ConstZero, TempOld
 bool ConstTrue, ConstFalse, TempBool
 
+
+
 ConstTrue  = true
 ConstFalse = false
 ConstZero  = 0
@@ -51,16 +53,16 @@ outputFile = open('Output.txt','w')
 print(startScript)
 outputFile.write(startScript+'\n')
 
-currStructName = 0
+currStructNumber = 0
 endStructs = len(structsList)
 for j in structsList:
     currNumVar = 0
-    if currStructName == 0:
-        str1 = 'if LcParameterSetNumber == ' + str(currStructName + 1) +' then'
+    if currStructNumber == 0:
+        str1 = 'if LcParameterSetNumber == ' + str(currStructNumber + 1) + ' then'
         print(str1)
         outputFile.write(str1+'\n')
     else:
-        str1 = 'else if LcParameterSetNumber == ' + str(currStructName + 1) +' then'
+        str1 = 'else if LcParameterSetNumber == ' + str(currStructNumber + 1) + ' then'
         print(str1)
         outputFile.write(str1+'\n')
     for i in enList:
@@ -68,35 +70,35 @@ for j in structsList:
             str1 = '    GetData(TempOld, "Local HMI", "'+ screenName + '_Old_' + varList[currNumVar] + '", 1)\n'
             str2 = '    GetData(TempShort, "Local HMI", "'+ screenName + '_' + varList[currNumVar] + '", 1)\n'
             str3 = '    if 	(TempOld <> TempShort) then\n'
-            str4 = '        SetData(TempShort, "Local HMI", "'+ structsList[currStructName] + '.' + varList[currNumVar] + '", 1)\n'
+            str4 = '        SetData(TempShort, "Local HMI", "'+ structsList[currStructNumber] + '.' + varList[currNumVar] + '", 1)\n'
             str5 = '    else\n'
-            str6 = '        GetData(TempShort, "Local HMI", "'+ structsList[currStructName] + '.' + varList[currNumVar] + '", 1)\n'
+            str6 = '        GetData(TempShort, "Local HMI", "'+ structsList[currStructNumber] + '.' + varList[currNumVar] + '", 1)\n'
             str7 = '        SetData(TempShort, "Local HMI", "'+ screenName + '_' + varList[currNumVar] + '", 1)\n'
             str8 = '    GetData(TempShort, "Local HMI", "'+ screenName + '_' + varList[currNumVar] + '", 1)\n'
             str9 = '    SetData(TempShort, "Local HMI", "'+ screenName + '_Old_' + varList[currNumVar] + '", 1)\n'
             print(str1+str2+str3+str4+str5+str6+str7+str8+str9)
             outputFile.write(str1+str2+str3+str4+str5+str6+str7+str8+str9+'\n')
         else:
-            str1 = '    GetData(TempShort, "Local HMI", "'+ structsList[currStructName] + '.' + varList[currNumVar] + '", 1)\n'
+            str1 = '    GetData(TempShort, "Local HMI", "'+ structsList[currStructNumber] + '.' + varList[currNumVar] + '", 1)\n'
             str2 = '    SetData(TempShort, "Local HMI", "'+ screenName + '_' + varList[currNumVar] + '", 1)\n'
             print(str1+str2)
             outputFile.write(str1+str2+'\n')
         currNumVar += 1
-    currStructName += 1
-    if currStructName == endStructs:
+    currStructNumber += 1
+    if currStructNumber == endStructs:
         str1 = 'else\n'
         print(str1)
         outputFile.write(str1+'\n')
 
-currStructName = 0
+currStructNumber = 0
 for j in structsList:
     currNumVar = 0
     for i in varList:
-        str1 = '    SetData(ConstZero, "Local HMI", "'+ structsList[currStructName] + '.' + varList[currNumVar] + '", 1)'
+        str1 = '    SetData(ConstZero, "Local HMI", "'+ structsList[currStructNumber] + '.' + varList[currNumVar] + '", 1)'
         print(str1)
         outputFile.write(str1+'\n')
         currNumVar += 1
-    currStructName += 1
+    currStructNumber += 1
 
 str1 = 'end if\n'
 str2 = 'end macro_command'
